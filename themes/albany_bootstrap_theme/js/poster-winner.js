42a171aa8e83d156e603f5df35add97e0a1b9631
abhinayTiwari <aitiwari@albany.edu>
2019-11-01 16:43:59 -0400

    updated winner.js and winner poster page

--- themes/albany_bootstrap_theme/js/poster-winner.js
+++ themes/albany_bootstrap_theme/js/poster-winner.js
@@ -1,4 +1,5 @@
-! function() { const e = (5, e => Object.keys(e).sort(function(t, n) { return e[n] - e[t] }).slice(0, 6));
-    window.risePosterWinners = function(t) { if ("object" != typeof t) return "Please send the JSON Object data"; if (!t.Items || 0 == t.Items.length) return "No Data Available yet!!"; const n = t.Items.map(e => e.ChosenPosters); return e(function(e) { const t = {},
-                n = { 1: 25, 2: 20, 3: 15, 4: 10, 5: 5 }; return e.forEach(function(e) { for (let o in e) { let s = n[e[o]]; if (t[o]) { let e = s + t[o];
-                        t[o] = e } else t[o] = s } }), t }(n)) } }();
+! function() { const e = function(e) { const t = {},
+                n = { 1: 25, 2: 20, 3: 15, 4: 10, 5: 5 }; return e.forEach(function(e) { for (let o in e) { let r = n[e[o]]; if (t[o]) { let e = r + t[o];
+                        t[o] = e } else t[o] = r } }), t },
+        t = (n = 5, e => Object.keys(e).sort(function(t, n) { return e[n] - e[t] }).slice(0, n)); var n;
+    window.risePosterWinners = function(n) { if ("object" != typeof n) return "Please send the JSON Object data"; if (!n.Items || 0 == n.Items.length) return "No Data Available yet!!"; const o = n.Items.map(e => e.ChosenPosters); return t(e(o)) } }();
